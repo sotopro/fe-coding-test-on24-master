cc443a5a1d35e0a6402d55881b4f6ca7691c8904
Daniel Soto <dasg.1993@gmail.com>
2022-05-06 19:41:40 -0500

    [feaature] - create button to select screen

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
 const styles = {
@@ -28,6 +28,7 @@ const styles = {
   optionsContainer: {
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
     flex: 0.1,
   },
   contentContainer: {
@@ -45,9 +46,37 @@ const styles = {
     border: "none",
     borderRight: "2px solid #ACD7EC",
   }),
+  customRadioButton: {
+    height: "32px",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    position: "relative",
+    backgroundColor: "#D6EDFF",
+  },
+  checkmark: (active) => ({
+    width: "25px",
+    height: "25px",
+    borderRadius: "50%",
+    display: "inline-block",
+    transition: "background-color 0.3s ease",
+    backgroundColor: active ? "#8B95C9" : "#D6EDFF",
+    border: `2px solid ${active ? "#D6EDFF" : "#8B95C9"}`,
+    padding: active ? "1px" : "3px",
+    top: 0,
+    left: 0,
+  }),
 };
 
 const Home = () => {
+  const [active, setActive] = useState(false);
+
+  const handleActiveRoute = (e) => {
+    e.preventDefault();
+    setActive(!active);
+  };
   return (
     <div style={styles.container}>
       <div style={styles.headerContiner}>
@@ -66,17 +95,14 @@ const Home = () => {
       </div>
       <div style={styles.contentContainer}>
         <div style={styles.optionsContainer}>
-          <button type="button" style={styles.optionsButton(true)}>
-            <svg height="40" width="40">
-              <circle
-                cx="20"
-                cy="20"
-                r="17"
-                stroke="#A9FFF7"
-                strokeWidth="1"
-                fill="#ffffff"
-              />
-            </svg>
+          <button
+            type="button"
+            style={styles.optionsButton(true)}
+            onClick={(e) => handleActiveRoute(e)}
+          >
+            <div style={styles.customRadioButton}>
+              <span style={styles.checkmark(active)} />
+            </div>
             <h2>Events</h2>
           </button>
           <button type="button" style={styles.optionsButton()}>
